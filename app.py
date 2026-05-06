9fbd552a4efd90b8845a776f5dd677eb3f376f2b
Saurabh Prajapati <saurabhp7@mobiquityinc.com>
2026-05-06 14:50:15 +0530

    fixed width of buttons

--- app.py
+++ app.py
@@ -49,6 +49,7 @@ else:
         st.sidebar.button(label, 
                           key=f"select_{row['account_id']}", 
                           type="primary" if selectedRow else "secondary",
+                          use_container_width=True,
                           on_click=select_account,
                           args=(row['account_id'],)
                           )
